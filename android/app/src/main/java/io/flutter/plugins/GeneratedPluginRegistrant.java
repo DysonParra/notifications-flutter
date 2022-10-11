158bb72244f523769938ea563ff8386463c2142b
Dyson <dysontilano@gmail.com>
2022-10-11 11:47:03 -0500

    Updated license of '*.java' files

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {FileName} se encarga de realizar tareas especificas.
+ * @fileoverview {GeneratedPluginRegistrant} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {FileName} fue realizada el 31/07/2022.
- * @Dev - La primera version de {FileName} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {GeneratedPluginRegistrant} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {GeneratedPluginRegistrant} fue escrita por Dyson A. Parra T.
  */
 package io.flutter.plugins;
 
